f3a18438d0c302b27b129532d8ed1e05772536cd
t1mch0w <timchou.hit@gmail.com>
2015-12-04 09:12:32 -0500

    First Commit

---
+++ Rest.java
@@ -0,0 +1,23 @@
+public class Rest {
+
+	public static void main(String [] args) {
+		BufferedReader br = new BufferedReader(new FileReader(args[1]));
+		// First, # of Dinners 
+		String line = br.readlin();
+		int numOfDin = Integer.parseInt(line);
+
+		// Second, # of Tables;
+		line = br.readlin();
+		int numOfTab = Integer.parseInt(line);
+
+		// Second, # of Tables;
+		line = br.readlin();
+		int numOfCook = Integer.parseInt(line);
+
+		for (int i = 0; i < numOfDim; i++) {
+
+		}
+
+	}
+
+}
